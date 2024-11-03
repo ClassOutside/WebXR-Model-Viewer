9c27f16d8f6c7c2c35111d2a970d81da74d1e02d
ClassOutside <132165158+ClassOutside@users.noreply.github.com>
2024-11-03 10:52:32 -0500

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,17 +1,39 @@
-import { defineConfig } from "vite";
+import { defineConfig, transformWithEsbuild } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [
+    {
+      name: "treat-js-files-as-jsx",
+      async transform(code, id) {
+        if (!id.match(/src\/.*\.js$/)) return null;
+
+        // Use the exposed transform from vite, instead of directly
+        // transforming with esbuild
+        return transformWithEsbuild(code, id, {
+          loader: "jsx",
+          jsx: "automatic",
+        });
+      },
+    },
+    react(),
+  ],
+
   server: {
     https: {
-      key: "./keys/localhost-key.pem",
-      cert: "./keys/localhost-cert.pem",
+      key: "./keys/key.pem",
+      cert: "./keys/cert.pem",
     },
     host: true,
     port: 3000,
   },
-  esbuild: {
-    include: /\.[jt]sx?$/,
-    exclude: [],
-    loader: "jsx",
+
+  optimizeDeps: {
+    force: true,
+    esbuildOptions: {
+      loader: {
+        ".js": "jsx",
+      },
+    },
   },
 });
